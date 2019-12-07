f7b091ede21cdf75ac31e8119f336ec2c939e3c8
cedcass <cassignac.cedric@orange.fr>
2019-12-07 20:26:28 +0100

    test restriction rules

--- src/main/java/mgb/muser/MuserApplication.java
+++ src/main/java/mgb/muser/MuserApplication.java
@@ -1,7 +1,7 @@
 /**
- *-------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
  * Copyright (C) 2019 QUI JE SUIS !!!. All rights reserved
- *-------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
  */
 package mgb.muser;
 
@@ -9,8 +9,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MuserApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MuserApplication.class, args);
-    }
+public class MuserApplication
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(MuserApplication.class, args);
+  }
 }
